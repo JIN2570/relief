e043cea3847c03fe0e2b7c52048e19091cb33e0b
JIN2570 <poll_down011@naver.com>
2023-09-27 21:41:22 +0900

    footer

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+new Swiper('.slide .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000 // 
+  },
+  slidesPerView: 1, 
+  spaceBetween: 10,
+  centeredSlides: true, 
+  pagination: { 
+    el: '.slide .swiper-pagination', 
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { 
+    nextEl: '.slide .swiper-btn-next', 
+    prevEl: '.slide .swiper-btn-prev'
+  }
+});
